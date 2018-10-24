71ae4451bb55a2429b447e64456de15225ca00c4
irshadvali <irshad.vali@gmail.com>
2018-10-24 07:36:03 +0530

    type relations

---
+++ other/commandText.js
@@ -0,0 +1,39 @@
+// {
+//     author(id:4) {
+//       name
+//       id
+//       age
+//     }
+//   }
+
+
+// {
+//     book(id: "4") {
+//       name
+//       genre
+//       id
+//     }
+//   }
+
+
+
+/*  type Relations
+
+
+
+{
+  book(id: "4") {
+    name
+    genre
+    id
+  author {
+    name
+    id
+    age
+  }
+  }
+}
+
+
+
+*/
